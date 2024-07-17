9090ba979e6a67cd70fc4873bae647c8fdf30e71
Linus Johansson <golfarenlj@gmail.com>
2024-07-17 21:20:43 +0000

    Add CurrentUserProvider for managing current user context

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CurrentUserProvider } from './contexts/CurrentUserContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <CurrentUserProvider>
+        <App />
+      </CurrentUserProvider>
     </Router>
   </React.StrictMode>,
 );
