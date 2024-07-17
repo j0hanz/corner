7c7a603b4d889ef61415a4eb109650516c000062
Linus Johansson <golfarenlj@gmail.com>
2024-07-17 21:20:35 +0000

    Add axios defaults for API requests

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './App.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
+import './api/axiosDefaults';
 
 function App() {
   return (
