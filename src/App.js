eb83fb4039629b22c6f644a39db43875c0467af2
Linus Johansson <golfarenlj@gmail.com>
2024-07-20 22:01:01 +0000

    Refactor App.js to improve code structure and remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
-import styles from './App.module.css';
+import { Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import UserPage from './pages/users/UserPage';
 import EditProfilePage from './pages/users/EditProfilePage';
-import { useCurrentUser } from './contexts/CurrentUserContext';
 
 function App() {
-  const currentUser = useCurrentUser();
   return (
     <div className={styles.App}>
       <NavBar />
-      <Routes>
-        <Route path="/users/:id" element={<UserPage />} />
-        <Route
-          path="/users/:id/edit"
-          element={
-            currentUser ? <EditProfilePage /> : <Navigate to="/" />
-          }
-        />
-      </Routes>
+      <main className={styles.main}>
+        <Routes>
+          <Route path="/users/:id" element={<UserPage />} />
+          <Route path="/users/:id/edit" element={<EditProfilePage />} />
+        </Routes>
+      </main>
     </div>
   );
 }
