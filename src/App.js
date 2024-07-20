0b53764f7da844ce5ee5f6689c353cd81075f76a
Linus Johansson <golfarenlj@gmail.com>
2024-07-20 21:15:08 +0000

    Add routes for UserPage and EditProfilePage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import styles from './App.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
-import './api/axiosDefaults';
+import UserPage from './pages/users/UserPage';
+import EditProfilePage from './pages/users/EditProfilePage';
+import { useCurrentUser } from './contexts/CurrentUserContext';
 
 function App() {
+  const currentUser = useCurrentUser();
   return (
     <div className={styles.App}>
       <NavBar />
+      <Routes>
+        <Route path="/users/:id" element={<UserPage />} />
+        <Route
+          path="/users/:id/edit"
+          element={
+            currentUser ? <EditProfilePage /> : <Navigate to="/" />
+          }
+        />
+      </Routes>
     </div>
   );
 }
