64bc452041bd6cc81d4982f00483ff53031d9129
Linus Johansson <golfarenlj@gmail.com>
2024-07-20 19:37:19 +0000

    Refactor Avatar component to improve code readability and maintainability

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import styles from './styles/Avatar.module.css';
 
-const Avatar = React.forwardRef(({ src, size, text }, ref) => (
-  <span ref={ref}>
-    <img
-      className={styles.Icon}
-      src={src}
-      height={size}
-      width={size}
-      alt="Avatar"
-    />
-    {text && <span className={styles.text}>{text}</span>}
-  </span>
-));
+
+const Avatar = React.forwardRef(({ src, height = 45, text }, ref) => {
+  return (
+    <span ref={ref}>
+      <img
+        className={styles.Icon}
+        src={src}
+        height={height}
+        width={height}
+        alt="Icon"
+      />
+      {text}
+    </span>
+  );
+});
 
 export default Avatar;
