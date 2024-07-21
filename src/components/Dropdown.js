96a9753c5ade5b48c5dc99fdb699e03d5240b7ce
Linus Johansson <golfarenlj@gmail.com>
2024-07-21 21:43:55 +0000

    Refactor Dropdown component to separate EditDeleteDropdown and ProfileActionsDropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -5,6 +5,11 @@ import {
   faEllipsis,
   faEdit,
   faTrashAlt,
+  faUserEdit,
+  faImage,
+  faUser,
+  faLock,
+  faUserSlash,
 } from '@fortawesome/free-solid-svg-icons';
 import styles from './styles/Dropdown.module.css';
 
@@ -20,21 +25,71 @@ const ThreeDots = React.forwardRef(({ onClick }, ref) => (
   />
 ));
 
-const CustomDropdown = ({ handleEdit, handleDelete }) => (
+const EditDeleteDropdown = ({ handleEdit, handleDelete }) => (
   <Dropdown drop="start" className="ms-auto">
-    <Dropdown.Toggle as={ThreeDots} id="dropdown-basic" />
+    <Dropdown.Toggle as={ThreeDots} id="edit-delete-dropdown" />
 
     <Dropdown.Menu className={styles.DropdownMenu}>
       <Dropdown.Item onClick={handleEdit} className={styles.DropdownItem}>
-        <span className={styles.faEdit}>Edit</span>{' '}
-        <FontAwesomeIcon icon={faEdit} className={styles.faEdit} />
+        <span>Edit</span>
+        <FontAwesomeIcon icon={faEdit} className={styles.Icon} />
       </Dropdown.Item>
       <Dropdown.Item onClick={handleDelete} className={styles.DropdownItem}>
-        <span className={styles.faTrashAlt}>Delete</span>{' '}
-        <FontAwesomeIcon icon={faTrashAlt} className={styles.faTrashAlt} />
+        <span>Delete</span>
+        <FontAwesomeIcon icon={faTrashAlt} className={styles.Icon} />
       </Dropdown.Item>
     </Dropdown.Menu>
   </Dropdown>
 );
 
-export default CustomDropdown;
+const ProfileActionsDropdown = ({
+  handleEditProfile,
+  handleChangeProfileImage,
+  handleChangeUsername,
+  handleChangePassword,
+  handleDeleteAccount,
+}) => (
+  <Dropdown drop="down">
+    <Dropdown.Toggle as={ThreeDots} id="profile-actions-dropdown" />
+
+    <Dropdown.Menu className={styles.DropdownMenu}>
+      <Dropdown.Item
+        onClick={handleEditProfile}
+        className={`text-white ${styles.DropdownItem}`}
+      >
+        <span>Edit Profile</span>
+        <FontAwesomeIcon icon={faUserEdit} className={styles.Icon} />
+      </Dropdown.Item>
+      <Dropdown.Item
+        onClick={handleChangeProfileImage}
+        className={styles.DropdownItem}
+      >
+        <span>Profile Image</span>
+        <FontAwesomeIcon icon={faImage} className={styles.Icon} />
+      </Dropdown.Item>
+      <Dropdown.Item
+        onClick={handleChangeUsername}
+        className={styles.DropdownItem}
+      >
+        <span>Change Username</span>
+        <FontAwesomeIcon icon={faUser} className={styles.Icon} />
+      </Dropdown.Item>
+      <Dropdown.Item
+        onClick={handleChangePassword}
+        className={styles.DropdownItem}
+      >
+        <span>Change Password</span>
+        <FontAwesomeIcon icon={faLock} className={styles.Icon} />
+      </Dropdown.Item>
+      <Dropdown.Item
+        onClick={handleDeleteAccount}
+        className={styles.DropdownItem}
+      >
+        <span>Delete Account</span>
+        <FontAwesomeIcon icon={faUserSlash} className={styles.Icon} />
+      </Dropdown.Item>
+    </Dropdown.Menu>
+  </Dropdown>
+);
+
+export { EditDeleteDropdown, ProfileActionsDropdown };
