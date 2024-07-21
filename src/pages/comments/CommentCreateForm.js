a1dc3c460601ac578b5fd4e17f2421705a073787
Linus Johansson <golfarenlj@gmail.com>
2024-07-21 03:40:41 +0000

    Add CommentCreateForm component for creating comments on a post

---
+++ src/pages/comments/CommentCreateForm.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { axiosRes } from '../../api/axiosDefaults';
+import Avatar from '../../components/Avatar';
+import styles from './styles/CommentCreateForm.module.css';
+
+function CommentCreateForm(props) {
+  const { post, setPost, setComments, profileImage, profile_id } = props;
+  const [content, setContent] = useState('');
+  const [commentErrorShow, setCommentErrorShow] = useState(false);
+
+  const handleChange = (event) => {
+    setContent(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axiosRes.post('/comments/', {
+        content,
+        post,
+      });
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: [data, ...prevComments.results],
+      }));
+      setPost((prevPost) => ({
+        results: [
+          {
+            ...prevPost.results[0],
+            comments_count: prevPost.results[0].comments_count + 1,
+          },
+        ],
+      }));
+      setContent('');
+    } catch (err) {
+      setCommentErrorShow(true);
+      setContent('');
+    }
+  };
+
+  return (
+    <>
+      <Row className="mb-3">
+        <Col className="d-flex align-items-center">
+          <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profileImage} height={30} />
+          </Link>
+          <Form onSubmit={handleSubmit} className="flex-grow-1 ms-2">
+            <Form.Group controlId="comment">
+              <Form.Label visuallyHidden>Leave your comment here</Form.Label>
+              <Form.Control
+                className={`${styles.CommentContent}`}
+                as="textarea"
+                placeholder="Leave your comment here"
+                rows={2}
+                value={content}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Button type="submit" variant="outline-light float-end">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+      {commentErrorShow && (
+        <div className="text-danger mt-2">
+          There was an error adding your comment!
+        </div>
+      )}
+    </>
+  );
+}
+
+export default CommentCreateForm;
