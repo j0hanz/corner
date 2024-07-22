5b49d89024b183d73e080e07303577250482d346
Linus Johansson <golfarenlj@gmail.com>
2024-07-22 19:38:54 +0000

    Refactor App.js to optimize rendering and remove unused code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import UserPage from './pages/users/UserPage';
-import EditProfilePage from './pages/users/EditProfilePage';
 import PostsFeed from './pages/posts/PostsFeed';
 import PostCreateForm from './pages/posts/PostCreateForm';
 import PostPage from './pages/posts/PostPage';
 import { useCurrentUser } from './contexts/CurrentUserContext';
 
-// Placeholder for PostEditForm
-const PostEditForm = () => <div>Post Edit Form</div>;
-
 function App() {
   const currentUser = useCurrentUser();
-  const profile_id = currentUser?.pk || '';
+  const profile_id = useMemo(() => currentUser?.pk || '', [currentUser]);
+
   return (
     <div className={styles.App}>
       <NavBar />
@@ -23,7 +20,6 @@ function App() {
         <Routes>
           <Route path="/" element={<PostsFeed message="No results found." />} />
           <Route path="/users/:id" element={<UserPage />} />
-          <Route path="/users/:id/edit" element={<EditProfilePage />} />
           <Route
             path="/liked-posts"
             element={
@@ -35,7 +31,6 @@ function App() {
           />
           <Route path="/posts/create" element={<PostCreateForm />} />
           <Route path="/posts/:id" element={<PostPage />} />
-          <Route path="/posts/:id/edit" element={<PostEditForm />} />
         </Routes>
       </main>
     </div>
