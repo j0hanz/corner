4df5f7b363a8ce7d4a625f1251cc05fea4544b42
Linus Johansson <golfarenlj@gmail.com>
2024-07-22 20:55:22 +0000

    Add "Create Post" button to PostsFeed page

--- src/pages/posts/PostsFeed.js
+++ src/pages/posts/PostsFeed.js
@@ -1,15 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import { Container, Row, Col, Form, Spinner, Alert } from 'react-bootstrap';
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  Spinner,
+  Alert,
+  Button,
+} from 'react-bootstrap';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import Post from './Post';
 import { axiosReq } from '../../api/axiosDefaults';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import PostCreateForm from './PostCreateForm'; // Adjust the import path as needed
 
 function PostsFeed({ message, filter = '' }) {
   const [posts, setPosts] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
   const [query, setQuery] = useState('');
+  const [showModal, setShowModal] = useState(false);
   const currentUser = useCurrentUser();
 
   useEffect(() => {
@@ -33,8 +45,22 @@ function PostsFeed({ message, filter = '' }) {
     };
   }, [filter, query, pathname, currentUser]);
 
+  const handleShowModal = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
+
   return (
     <Container>
+      <Row className="justify-content-center">
+        <Col xs={12} xl={10} className="text-center">
+          <Button
+            variant="outline-light"
+            className="my-1"
+            onClick={handleShowModal}
+          >
+            <FontAwesomeIcon icon={faPlus} className="fa-lg" />
+          </Button>
+        </Col>
+      </Row>
       <Row className="justify-content-center my-4">
         <Col xs={12} xl={10}>
           <Form onSubmit={(event) => event.preventDefault()}>
@@ -65,6 +91,7 @@ function PostsFeed({ message, filter = '' }) {
           )}
         </Col>
       </Row>
+      <PostCreateForm show={showModal} handleClose={handleCloseModal} />
     </Container>
   );
 }
