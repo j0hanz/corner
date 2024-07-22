1446493f6d38b9b3d154ccb94a103b5911f4caa6
Linus Johansson <golfarenlj@gmail.com>
2024-07-22 19:39:46 +0000

    Refactor ChangePasswordModal component for improved user experience

--- src/pages/users/ChangePasswordModal.js
+++ src/pages/users/ChangePasswordModal.js
@@ -1,33 +1,48 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Modal, Button, Form, Alert, Container } from 'react-bootstrap';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { axiosRes } from '../../api/axiosDefaults';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import styles from './styles/EditProfilePage.module.css';
 
 const ChangePasswordModal = ({ show, handleClose }) => {
+  const navigate = useNavigate();
   const { id } = useParams();
-  const [password, setPassword] = useState('');
+  const currentUser = useCurrentUser();
+
+  const [userData, setUserData] = useState({
+    new_password1: '',
+    new_password2: '',
+  });
+  const { new_password1, new_password2 } = userData;
   const [errors, setErrors] = useState({});
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
+  const handleChange = (event) => {
+    setUserData({
+      ...userData,
+      [event.target.name]: event.target.value,
+    });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    axios
-      .put(`/users/${id}/`, { password })
-      .then(() => {
-        handleClose();
-        window.location.reload();
-      })
-      .catch((error) => {
-        setErrors(error.response?.data || {});
-      });
+  useEffect(() => {
+    if (currentUser?.pk?.toString() !== id) {
+      navigate('/');
+    }
+  }, [currentUser, navigate, id]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axiosRes.post('/dj-rest-auth/password/change/', userData);
+      handleClose();
+      window.location.reload();
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
   };
 
   return (
-    <Modal show={show} onHide={handleClose} className="text-light">
+    <Modal show={show} onHide={handleClose} centered className="text-light">
       <Modal.Header
         closeButton
         closeVariant="white"
@@ -35,33 +50,54 @@ const ChangePasswordModal = ({ show, handleClose }) => {
       >
         <Modal.Title>Change Password</Modal.Title>
       </Modal.Header>
-      <Modal.Body className="bg-dark text-light">
+      <Modal.Body className="bg-dark text-light p-0">
         <Container className={styles.Container}>
           {errors.detail && <Alert variant="danger">{errors.detail}</Alert>}
           <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="formPassword" className="mb-3">
+            <Form.Group controlId="formNewPassword1" className="mb-3">
               <Form.Label>New Password</Form.Label>
               <Form.Control
                 type="password"
                 placeholder="Enter new password"
-                value={password}
-                onChange={handlePasswordChange}
+                name="new_password1"
+                value={new_password1}
+                onChange={handleChange}
                 className="bg-dark text-light"
               />
-              {errors?.password?.map((message, idx) => (
+              {errors?.new_password1?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                  {message}
+                </Alert>
+              ))}
+            </Form.Group>
+            <Form.Group controlId="formNewPassword2" className="mb-3">
+              <Form.Label>Confirm New Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Confirm new password"
+                name="new_password2"
+                value={new_password2}
+                onChange={handleChange}
+                className="bg-dark text-light"
+              />
+              {errors?.new_password2?.map((message, idx) => (
                 <Alert variant="warning" key={idx}>
                   {message}
                 </Alert>
               ))}
             </Form.Group>
             <div className={styles.buttonWrapper}>
-              <Button variant="primary" type="submit" className="mx-2">
+              <Button
+                variant="outline-primary"
+                type="submit"
+                className={styles.leftButton}
+              >
                 Save Changes
               </Button>
               <Button
-                variant="secondary"
+                variant="outline-secondary"
                 onClick={handleClose}
-                className="mx-2"
+                className={styles.rightButton}
               >
                 Cancel
               </Button>
@@ -69,11 +105,6 @@ const ChangePasswordModal = ({ show, handleClose }) => {
           </Form>
         </Container>
       </Modal.Body>
-      <Modal.Footer className="bg-dark text-light">
-        <Button variant="secondary" onClick={handleClose}>
-          Close
-        </Button>
-      </Modal.Footer>
     </Modal>
   );
 };
