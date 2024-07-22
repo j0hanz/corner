618e699098744742b859912f350734d94fdde3bf
Linus Johansson <golfarenlj@gmail.com>
2024-07-22 10:20:11 +0000

    Refactor routing for user profile image upload

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import UserPage from './pages/users/UserPage';
 import EditProfilePage from './pages/users/EditProfilePage';
-import ProfileImageUpload from './pages/users/ProfileImageUpload';
 import PostsFeed from './pages/posts/PostsFeed';
 import PostCreateForm from './pages/posts/PostCreateForm';
 import PostPage from './pages/posts/PostPage';
@@ -25,10 +24,6 @@ function App() {
           <Route path="/" element={<PostsFeed message="No results found." />} />
           <Route path="/users/:id" element={<UserPage />} />
           <Route path="/users/:id/edit" element={<EditProfilePage />} />
-          <Route
-            path="/users/:id/profile-image"
-            element={<ProfileImageUpload />}
-          />
           <Route
             path="/liked-posts"
             element={
