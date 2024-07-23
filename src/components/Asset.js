63989750f96054304efad55741ac7c9341ad57df
Linus Johansson <golfarenlj@gmail.com>
2024-07-23 22:27:20 +0000

    Refactor Asset component to improve code readability.

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -3,12 +3,9 @@ import { Spinner } from 'react-bootstrap';
 import styles from './styles/Asset.module.css';
 
 const Asset = ({ spinner, src, message }) => {
-  if (spinner) {
-    return <Spinner animation="border" variant="primary" />;
-  }
-
   return (
     <div className={`${styles.Asset} text-center`}>
+      {spinner && <Spinner animation="border" variant="primary" />}
       {src && <img src={src} alt="No results" className="img-fluid" />}
       {message && <p className="mt-2">{message}</p>}
     </div>
