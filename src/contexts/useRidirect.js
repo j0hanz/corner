6745bddd27b0f13d7cbd2d83657a35b8ed466b35
Linus Johansson <golfarenlj@gmail.com>
2024-07-23 22:26:31 +0000

    Refactor useRedirect

--- src/contexts/useRidirect.js
+++ src/contexts/useRidirect.js
@@ -9,12 +9,12 @@ export const useRedirect = (userAuthStatus) => {
     const handleMount = async () => {
       try {
         await axios.post('/dj-rest-auth/token/refresh/');
-        if (userAuthStatus === 'loggedIn') {
-          navigate('/');
-        }
-      } catch (err) {
         if (userAuthStatus === 'loggedOut') {
           navigate('/login');
+        }
+      } catch (err) {
+        if (userAuthStatus === 'loggedIn') {
+          navigate('/');
         }
       }
     };
