e7bf2b75a2acfaab19dad18b476b51d12c48d387
Linus Johansson <golfarenlj@gmail.com>
2024-07-23 22:30:42 +0000

    Refactor Avatar component and improve code readability

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import styles from './styles/Avatar.module.css';
 
-
 const Avatar = React.forwardRef(({ src, height = 45, text }, ref) => {
   return (
-    <span ref={ref}>
+    <span ref={ref} className={styles.Avatar}>
       <img
         className={styles.Icon}
         src={src}
         height={height}
         width={height}
-        alt="Icon"
+        alt="Avatar"
       />
-      {text}
+      {text && <span className={styles.Text}>{text}</span>}
     </span>
   );
 });
