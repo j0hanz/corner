61911bd03cca211eb144748ae85edc6f9cc47608
Linus Johansson <golfarenlj@gmail.com>
2024-07-24 10:45:08 +0000

    Add loading spinner to ChangeUsernameModal.js

--- src/pages/users/ChangeUsernameModal.js
+++ src/pages/users/ChangeUsernameModal.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Modal, Button, Form, Alert, Container } from 'react-bootstrap';
+import {
+  Modal,
+  Button,
+  Form,
+  Alert,
+  Container,
+  Spinner,
+} from 'react-bootstrap';
 import { axiosRes } from '../../api/axiosDefaults';
 import {
   useCurrentUser,
   useSetCurrentUser,
 } from '../../contexts/CurrentUserContext';
 import styles from './styles/EditProfilePage.module.css';
-import LoadingSpinnerToast from '../../components/LoadingSpinnerToast';
 
 const ChangeUsernameModal = ({ show, handleClose }) => {
   const [username, setUsername] = useState('');
@@ -42,62 +48,67 @@ const ChangeUsernameModal = ({ show, handleClose }) => {
   };
 
   return (
-    <>
-      <Modal show={show} onHide={handleClose} centered className="text-light">
-        <Modal.Header
-          closeButton
-          closeVariant="white"
-          className="bg-dark text-light"
-        >
-          <Modal.Title>Change Username</Modal.Title>
-        </Modal.Header>
-        <Modal.Body className="bg-dark text-light p-0">
-          <Container className={styles.Container}>
-            {errors.detail && <Alert variant="danger">{errors.detail}</Alert>}
-            <Form onSubmit={handleSubmit}>
-              <Form.Group controlId="formUsername" className="mb-3">
-                <Form.Label>New Username</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Enter new username"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                  className="bg-dark text-light"
-                />
-                {errors?.username?.map((message, idx) => (
-                  <Alert variant="warning" key={idx}>
-                    {message}
-                  </Alert>
-                ))}
-              </Form.Group>
-              <div className={styles.buttonWrapper}>
-                <Button
-                  variant="outline-primary"
-                  type="submit"
-                  className={styles.leftButton}
-                >
-                  Save Changes
-                </Button>
-                <Button
-                  variant="outline-secondary"
-                  onClick={handleClose}
-                  className={styles.rightButton}
-                >
-                  Cancel
-                </Button>
-              </div>
-              {loading && (
-                <LoadingSpinnerToast
-                  show={true}
-                  message="Processing, please wait..."
-                  duration={5000}
-                />
-              )}
-            </Form>
-          </Container>
-        </Modal.Body>
-      </Modal>
-    </>
+    <Modal show={show} onHide={handleClose} centered className="text-light">
+      <Modal.Header
+        closeButton
+        closeVariant="white"
+        className="bg-dark text-light"
+      >
+        <Modal.Title>Change Username</Modal.Title>
+      </Modal.Header>
+      <Modal.Body className="bg-dark text-light p-0">
+        <Container className={styles.Container}>
+          {errors.detail && <Alert variant="danger">{errors.detail}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formUsername" className="mb-3">
+              <Form.Label>New Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter new username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="bg-dark text-light"
+              />
+              {errors?.username?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                  {message}
+                </Alert>
+              ))}
+            </Form.Group>
+            <div className={styles.buttonWrapper}>
+              <Button
+                variant="outline-primary"
+                type="submit"
+                disabled={loading}
+              >
+                {loading ? (
+                  <>
+                    <Spinner
+                      as="span"
+                      animation="border"
+                      size="sm"
+                      role="status"
+                      aria-hidden="true"
+                      style={{ color: 'white' }}
+                    />{' '}
+                    <span className="text-light">Saving...</span>
+                  </>
+                ) : (
+                  'Save Changes'
+                )}
+              </Button>
+              <Button
+                variant="outline-secondary"
+                onClick={handleClose}
+                disabled={loading}
+              >
+                Cancel
+              </Button>
+            </div>
+          </Form>
+        </Container>
+      </Modal.Body>
+    </Modal>
   );
 };
 
