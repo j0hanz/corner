222799b675fc27175d739846264b355e7055f8ff
Linus Johansson <golfarenlj@gmail.com>
2024-07-24 10:38:29 +0000

    Remove spinner prop from Asset component

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Spinner } from 'react-bootstrap';
 import styles from './styles/Asset.module.css';
 
-const Asset = ({ spinner, src, message }) => {
+const Asset = ({ src, message }) => {
   return (
     <div className={`${styles.Asset} text-center`}>
-      {spinner && <Spinner animation="border" variant="primary" />}
       {src && <img src={src} alt="No results" className="img-fluid" />}
       {message && <p className="mt-2">{message}</p>}
     </div>
