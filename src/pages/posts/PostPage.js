8d52c80b5b945c37533cd16e12c2975b53dab815
Linus Johansson <golfarenlj@gmail.com>
2024-07-24 10:53:11 +0000

    Refactor PostPage component.

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -39,64 +39,66 @@ const PostPage = ({ show, handleClose, postId }) => {
   }, [show, fetchPostAndComments]);
 
   return (
-    <Modal show={show} onHide={handleClose} centered>
-      {!hasLoaded && (
-        <LoadingSpinnerToast
-          show={true}
-          message="Loading post and comments, please wait..."
-          duration={5000}
-        />
-      )}
-      {hasLoaded && (
-        <>
-          <Modal.Header
-            closeButton
-            closeVariant="white"
-            className="bg-dark text-light"
-          >
-            <Modal.Title>Post</Modal.Title>
-          </Modal.Header>
-          <Modal.Body className="bg-dark text-light p-0">
-            <Container className={styles.Container}>
-              <Row className="justify-content-center my-4">
-                <Col xs={12} xl={10}>
-                  <Post {...post.results[0]} setPosts={setPost} postPage />
-                  <hr />
-                  {currentUser && (
-                    <CommentCreateForm
-                      profile_id={currentUser.profile_id}
-                      profileImage={profileImage}
-                      post={postId}
-                      setPost={setPost}
-                      setComments={setComments}
-                    />
-                  )}
-                  <hr />
-                  {comments.results.length ? (
-                    <InfiniteScroll
-                      dataLength={comments.results.length}
-                      next={() => fetchMoreData(comments, setComments)}
-                      hasMore={!!comments.next}
-                    >
-                      {comments.results.map((comment) => (
-                        <Comment
-                          key={comment.id}
-                          {...comment}
-                          setPost={setPost}
-                          setComments={setComments}
-                        />
-                      ))}
-                    </InfiniteScroll>
-                  ) : (
-                    <div className="text-center">No comments yet.</div>
-                  )}
-                </Col>
-              </Row>
-            </Container>
-          </Modal.Body>
-        </>
-      )}
-    </Modal>
+    <>
+      <Modal show={show} onHide={handleClose} centered>
+        {!hasLoaded && (
+          <LoadingSpinnerToast
+            show={true}
+            message="Loading post and comments, please wait..."
+            duration={5000}
+          />
+        )}
+        {hasLoaded && (
+          <>
+            <Modal.Header
+              closeButton
+              closeVariant="white"
+              className="bg-dark text-light"
+            >
+              <Modal.Title>Post</Modal.Title>
+            </Modal.Header>
+            <Modal.Body className="bg-dark text-light p-0">
+              <Container className={styles.Container}>
+                <Row className="justify-content-center my-4">
+                  <Col xs={12} xl={10}>
+                    <Post {...post.results[0]} setPosts={setPost} postPage />
+                    <hr />
+                    {currentUser && (
+                      <CommentCreateForm
+                        profile_id={currentUser.profile_id}
+                        profileImage={profileImage}
+                        post={postId}
+                        setPost={setPost}
+                        setComments={setComments}
+                      />
+                    )}
+                    <hr />
+                    {comments.results.length ? (
+                      <InfiniteScroll
+                        dataLength={comments.results.length}
+                        next={() => fetchMoreData(comments, setComments)}
+                        hasMore={!!comments.next}
+                      >
+                        {comments.results.map((comment) => (
+                          <Comment
+                            key={comment.id}
+                            {...comment}
+                            setPost={setPost}
+                            setComments={setComments}
+                          />
+                        ))}
+                      </InfiniteScroll>
+                    ) : (
+                      <div className="text-center">No comments yet.</div>
+                    )}
+                  </Col>
+                </Row>
+              </Container>
+            </Modal.Body>
+          </>
+        )}
+      </Modal>
+    </>
   );
 };
 
