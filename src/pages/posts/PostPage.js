4b45ad63cb51f28fcde442f02944f2aeafa47976
Linus Johansson <golfarenlj@gmail.com>
2024-07-24 11:14:25 +0000

    Add loading spinner to PostPage component

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -13,22 +13,26 @@ import styles from './styles/PostPage.module.css';
 const PostPage = ({ show, handleClose, postId }) => {
   const [post, setPost] = useState({ results: [] });
   const [comments, setComments] = useState({ results: [] });
-  const [hasLoaded, setHasLoaded] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   const currentUser = useCurrentUser();
   const profileImage = currentUser?.profile_image;
 
   const fetchPostAndComments = useCallback(async () => {
     try {
-      setHasLoaded(false);
+      setLoading(true);
+      setError(false);
       const [{ data: post }, { data: comments }] = await Promise.all([
         axiosReq.get(`/posts/${postId}`),
         axiosReq.get(`/comments/?post=${postId}`),
       ]);
       setPost({ results: [post] });
       setComments(comments);
-      setHasLoaded(true);
     } catch (err) {
+      setError(true);
       console.error(err);
+    } finally {
+      setLoading(false);
     }
   }, [postId]);
 
@@ -39,31 +43,35 @@ const PostPage = ({ show, handleClose, postId }) => {
   }, [show, fetchPostAndComments]);
 
   return (
-    <>
-      <Modal show={show} onHide={handleClose} centered>
-        {!hasLoaded && (
-          <LoadingSpinnerToast
-            show={true}
-            message="Loading post and comments, please wait..."
-            duration={5000}
-          />
-        )}
-        {hasLoaded && (
-          <>
-            <Modal.Header
-              closeButton
-              closeVariant="white"
-              className="bg-dark text-light"
-            >
-              <Modal.Title>Post</Modal.Title>
-            </Modal.Header>
-            <Modal.Body className="bg-dark text-light p-0">
-              <Container className={styles.Container}>
-                <Row className="justify-content-center my-4">
-                  <Col xs={12} xl={10}>
-                    <Post {...post.results[0]} setPosts={setPost} postPage />
-                    <hr />
-                    {currentUser && (
+    <Modal show={show} onHide={handleClose} centered>
+      {loading ? (
+        <LoadingSpinnerToast
+          show={true}
+          message="Loading post and comments, please wait..."
+          duration={5000}
+        />
+      ) : error ? (
+        <div className="text-center my-4 text-danger">
+          Failed to load post and comments.
+        </div>
+      ) : (
+        <>
+          <Modal.Header
+            closeButton
+            closeVariant="white"
+            className="bg-dark text-light"
+          >
+            <Modal.Title>Post</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body className="bg-dark text-light p-0">
+            <Container fluid className={`${styles.Container} p-1`}>
+              <Row className="justify-content-center">
+                <Col xs={12} xl={10}>
+                  <Post {...post.results[0]} setPosts={setPost} postPage />
+                  <hr />
+                  {currentUser && (
+                    <>
                       <CommentCreateForm
                         profile_id={currentUser.profile_id}
                         profileImage={profileImage}
@@ -71,34 +79,40 @@ const PostPage = ({ show, handleClose, postId }) => {
                         setPost={setPost}
                         setComments={setComments}
                       />
-                    )}
-                    <hr />
-                    {comments.results.length ? (
-                      <InfiniteScroll
-                        dataLength={comments.results.length}
-                        next={() => fetchMoreData(comments, setComments)}
-                        hasMore={!!comments.next}
-                      >
-                        {comments.results.map((comment) => (
-                          <Comment
-                            key={comment.id}
-                            {...comment}
-                            setPost={setPost}
-                            setComments={setComments}
-                          />
-                        ))}
-                      </InfiniteScroll>
-                    ) : (
-                      <div className="text-center">No comments yet.</div>
-                    )}
-                  </Col>
-                </Row>
-              </Container>
-            </Modal.Body>
-          </>
-        )}
-      </Modal>
-    </>
+                      <hr />
+                    </>
+                  )}
+                  {comments.results.length > 0 ? (
+                    <InfiniteScroll
+                      dataLength={comments.results.length}
+                      next={() => fetchMoreData(comments, setComments)}
+                      hasMore={!!comments.next}
+                      loader={<LoadingSpinnerToast show={true} />}
+                      endMessage={
+                        <p className="text-center text-white-50 my-4">
+                          No more comments
+                        </p>
+                      }
+                    >
+                      {comments.results.map((comment) => (
+                        <Comment
+                          key={comment.id}
+                          {...comment}
+                          setPost={setPost}
+                          setComments={setComments}
+                        />
+                      ))}
+                    </InfiniteScroll>
+                  ) : (
+                    <div className="text-center my-4">No comments yet.</div>
+                  )}
+                </Col>
+              </Row>
+            </Container>
+          </Modal.Body>
+        </>
+      )}
+    </Modal>
   );
 };
 
