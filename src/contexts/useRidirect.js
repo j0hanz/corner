8c79db26de18f7f15070a76da4e3ab55fb92efa8
Linus Johansson <golfarenlj@gmail.com>
2024-07-27 21:58:45 +0000

    Update navigate path in useRedirect.js to fix redirection issue

--- src/contexts/useRidirect.js
+++ src/contexts/useRidirect.js
@@ -10,7 +10,7 @@ export const useRedirect = (userAuthStatus) => {
       try {
         await axios.post('/dj-rest-auth/token/refresh/');
         if (userAuthStatus === 'loggedOut') {
-          navigate('/login');
+          navigate('/');
         }
       } catch (err) {
         if (userAuthStatus === 'loggedIn') {
