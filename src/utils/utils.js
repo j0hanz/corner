e706a7aac23b3647daf9525164e4c686eb195b39
Linus Johansson <golfarenlj@gmail.com>
2024-07-29 23:13:01 +0000

    Refactor fetchMoreData function to optimize performance and improve code readability

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -5,16 +5,12 @@ export const fetchMoreData = async (resource, setResource) => {
   try {
     const { data } = await axiosReq.get(resource.next);
     const { next, results } = data;
+    const existingPostIds = new Set(resource.results.map((post) => post.id));
+    const newResults = results.filter((post) => !existingPostIds.has(post.id));
     setResource((prevResource) => ({
       ...prevResource,
       next,
-      results: [
-        ...prevResource.results,
-        ...results.filter(
-          (cur) =>
-            !prevResource.results.some((accResult) => accResult.id === cur.id)
-        ),
-      ],
+      results: [...prevResource.results, ...newResults],
     }));
   } catch (err) {
     console.error('Error fetching more data:', err);
