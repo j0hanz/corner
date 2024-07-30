5c948b4a71a262c72ac081010bf84ce400090a9c
Linus Johansson <golfarenlj@gmail.com>
2024-07-30 15:31:42 +0000

    Refactor profile data update functions for follow/unfollow functionality

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -16,3 +16,39 @@ export const fetchMoreData = async (resource, setResource) => {
     console.error('Error fetching more data:', err);
   }
 };
+
+/* Helper function to update profile data when a user is followed */
+export const followHelper = (profile, clickedProfile, followingId) => {
+  if (profile.id === clickedProfile.id) {
+    return {
+      ...profile,
+      followers_count: profile.followers_count + 1,
+      following_id: followingId,
+    };
+  }
+  if (profile.is_owner) {
+    return {
+      ...profile,
+      following_count: profile.following_count + 1,
+    };
+  }
+  return profile;
+};
+
+/* Helper function to update profile data when a user is unfollowed */
+export const unfollowHelper = (profile, clickedProfile) => {
+  if (profile.id === clickedProfile.id) {
+    return {
+      ...profile,
+      followers_count: profile.followers_count - 1,
+      following_id: null,
+    };
+  }
+  if (profile.is_owner) {
+    return {
+      ...profile,
+      following_count: profile.following_count - 1,
+    };
+  }
+  return profile;
+};
