68648c782765aadaa401c423c5246e16ceaae62d
Linus Johansson <golfarenlj@gmail.com>
2024-07-30 16:41:53 +0000

    Refactor follow/unfollow functionality in Profile component and related code

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -18,37 +18,27 @@ export const fetchMoreData = async (resource, setResource) => {
 };
 
 /* Helper function to update profile data when a user is followed */
-export const followHelper = (profile, clickedProfile, followingId) => {
-  if (profile.id === clickedProfile.id) {
-    return {
-      ...profile,
-      followers_count: profile.followers_count + 1,
-      following_id: followingId,
-    };
-  }
-  if (profile.is_owner) {
-    return {
-      ...profile,
-      following_count: profile.following_count + 1,
-    };
-  }
-  return profile;
+export const followHelper = (profile, clickedProfile, following_id) => {
+  return profile.id === clickedProfile.id
+    ? {
+        ...profile,
+        followers_count: profile.followers_count + 1,
+        following_id,
+      }
+    : profile.is_owner
+      ? { ...profile, following_count: profile.following_count + 1 }
+      : profile;
 };
 
 /* Helper function to update profile data when a user is unfollowed */
 export const unfollowHelper = (profile, clickedProfile) => {
-  if (profile.id === clickedProfile.id) {
-    return {
-      ...profile,
-      followers_count: profile.followers_count - 1,
-      following_id: null,
-    };
-  }
-  if (profile.is_owner) {
-    return {
-      ...profile,
-      following_count: profile.following_count - 1,
-    };
-  }
-  return profile;
+  return profile.id === clickedProfile.id
+    ? {
+        ...profile,
+        followers_count: profile.followers_count - 1,
+        following_id: null,
+      }
+    : profile.is_owner
+      ? { ...profile, following_count: profile.following_count - 1 }
+      : profile;
 };
