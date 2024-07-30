de06f70854d37ba46d509b1ed230398ad3dacce5
Linus Johansson <golfarenlj@gmail.com>
2024-07-30 15:30:55 +0000

    Refactor CurrentUserProvider to include ProfileDataProvider for better data management

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { CurrentUserProvider } from './contexts/CurrentUserContext';
+import { ProfileDataProvider } from './contexts/ProfileDataContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
       <CurrentUserProvider>
-        <App />
+        <ProfileDataProvider>
+          <App />
+        </ProfileDataProvider>
       </CurrentUserProvider>
     </Router>
   </React.StrictMode>
