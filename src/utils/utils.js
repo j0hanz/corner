4b7bc55342d79e992ab6b41cb0857db51ef13f0b
Linus Johansson <golfarenlj@gmail.com>
2024-07-31 20:14:20 +0000

    Refactor follow/unfollow functionality in ProfileDataContext and Profile component

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,44 +1,90 @@
 import { axiosReq } from '../api/axiosDefaults';
+import PropTypes from 'prop-types';
+
+// Constants for profile properties
+const PROFILE_ID = 'id';
+const FOLLOWERS_COUNT = 'followers_count';
+const FOLLOWING_COUNT = 'following_count';
+const IS_OWNER = 'is_owner';
 
 export const fetchMoreData = async (resource, setResource) => {
   if (!resource.next) return;
+
   try {
     const { data } = await axiosReq.get(resource.next);
     const { next, results } = data;
     const existingPostIds = new Set(resource.results.map((post) => post.id));
     const newResults = results.filter((post) => !existingPostIds.has(post.id));
+
     setResource((prevResource) => ({
       ...prevResource,
       next,
       results: [...prevResource.results, ...newResults],
     }));
   } catch (err) {
-    console.error('Error fetching more data:', err);
+    if (err.response) {
+      console.error(
+        'Server responded with an error:',
+        err.response.status,
+        err.response.data
+      );
+    } else if (err.request) {
+      console.error('No response received:', err.request);
+    } else {
+      console.error('Error setting up request:', err.message);
+    }
   }
 };
 
+fetchMoreData.propTypes = {
+  resource: PropTypes.shape({
+    next: PropTypes.string,
+    results: PropTypes.arrayOf(PropTypes.object),
+  }).isRequired,
+  setResource: PropTypes.func.isRequired,
+};
+
 /* Helper function to update profile data when a user is followed */
-export const followHelper = (profile, clickedProfile, following_id) => {
-  return profile.id === clickedProfile.id
-    ? {
-        ...profile,
-        followers_count: profile.followers_count + 1,
-        following_id,
-      }
-    : profile.is_owner
-      ? { ...profile, following_count: profile.following_count + 1 }
-      : profile;
+export const updateProfileOnFollow = (
+  profile,
+  clickedProfile,
+  following_id
+) => {
+  if (profile[PROFILE_ID] === clickedProfile[PROFILE_ID]) {
+    return {
+      ...profile,
+      [FOLLOWERS_COUNT]: profile[FOLLOWERS_COUNT] + 1,
+      following_id,
+    };
+  } else if (profile[IS_OWNER]) {
+    return { ...profile, [FOLLOWING_COUNT]: profile[FOLLOWING_COUNT] + 1 };
+  } else {
+    return profile;
+  }
+};
+
+updateProfileOnFollow.propTypes = {
+  profile: PropTypes.object.isRequired,
+  clickedProfile: PropTypes.object.isRequired,
+  following_id: PropTypes.number.isRequired,
 };
 
 /* Helper function to update profile data when a user is unfollowed */
-export const unfollowHelper = (profile, clickedProfile) => {
-  return profile.id === clickedProfile.id
-    ? {
-        ...profile,
-        followers_count: profile.followers_count - 1,
-        following_id: null,
-      }
-    : profile.is_owner
-      ? { ...profile, following_count: profile.following_count - 1 }
-      : profile;
+export const updateProfileOnUnfollow = (profile, clickedProfile) => {
+  if (profile[PROFILE_ID] === clickedProfile[PROFILE_ID]) {
+    return {
+      ...profile,
+      [FOLLOWERS_COUNT]: profile[FOLLOWERS_COUNT] - 1,
+      following_id: null,
+    };
+  } else if (profile[IS_OWNER]) {
+    return { ...profile, [FOLLOWING_COUNT]: profile[FOLLOWING_COUNT] - 1 };
+  } else {
+    return profile;
+  }
+};
+
+updateProfileOnUnfollow.propTypes = {
+  profile: PropTypes.object.isRequired,
+  clickedProfile: PropTypes.object.isRequired,
 };
