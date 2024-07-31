c331d137279480500d816d17bc83943083dd9db8
Linus Johansson <golfarenlj@gmail.com>
2024-07-31 21:40:41 +0000

    Refactor for better readability and styling

--- src/pages/users/PopularProfiles.js
+++ src/pages/users/PopularProfiles.js
@@ -8,26 +8,29 @@ import styles from './styles/PopularProfiles.module.css';
 /* PopularProfiles component to display a list of popular profiles */
 const PopularProfiles = () => {
   const { popularProfiles } = useProfileData();
+  const { results } = popularProfiles;
+
+  const renderProfiles = () => (
+    <div className={styles.ProfilesSection}>
+      <hr />
+      <div className={`text-center py-3 ${styles.Header}`}>
+        <h3>Most Followed Profiles</h3>
+      </div>
+      <div
+        className={`d-flex flex-wrap justify-content-around bg-dark py-4 ${styles.PopularProfilesBorder}`}
+      >
+        {results.slice(0, 4).map((profile) => (
+          <Profile key={profile.id} profile={profile} />
+        ))}
+      </div>
+      <hr />
+    </div>
+  );
 
   return (
-    <Container>
-      {popularProfiles.results.length ? (
-        <>
-          <div className={styles.ProfilesSection}>
-            <hr />
-            <div className={`text-center py-3 ${styles.Header}`}>
-              <h3>Most Followed Profiles</h3>
-            </div>
-            <div
-              className={`d-flex flex-wrap justify-content-around bg-dark py-4 ${styles.PopularProfilesBorder}`}
-            >
-              {popularProfiles.results.slice(0, 4).map((profile) => (
-                <Profile key={profile.id} profile={profile} />
-              ))}
-            </div>
-            <hr />
-          </div>
-        </>
+    <Container className="px-0">
+      {results.length ? (
+        renderProfiles()
       ) : (
         <div className="d-flex justify-content-center">
           <Asset spinner />
